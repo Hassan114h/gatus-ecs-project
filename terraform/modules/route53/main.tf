8deea447ae3e556bf2bbdda14c249c520be3e0fc
Hassan <hassanhagi93@gmail.com>
2025-08-19 15:35:26 +0100

    Fixing route53

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -24,14 +24,14 @@ resource "aws_acm_certificate_validation" "acm_cert" {
   }
 }
 
-resource "aws_route53_record" "alb_alias" {
-  zone_id = var.alb_hosted_zone_id
-  name    = var.alb_dns_name
+resource "aws_route53_record" "gatus" {
+  zone_id = var.hosted_zone_id
+  name    = var.domain_name        
   type    = "A"
 
   alias {
-    name                   = var.alb_dns_name
-    zone_id                = var.alb_hosted_zone_id
+    name                   = var.alb_dns_name      
+    zone_id                = var.alb_hosted_zone_id       
     evaluate_target_health = true
   }
 }
--- terraform/modules/route53/variables.tf
+++ terraform/modules/route53/variables.tf
@@ -1,11 +1,21 @@
-variable "alb_dns_name" {
+variable "domain_name" {
   description = "The DNS name of the ALB"
   type        = string
   default = "hassan114.click"
 }
 
-variable "alb_hosted_zone_id" {
+variable "hosted_zone_id" {
   description = "The hosted zone ID of the ALB"
   type        = string
   default = "Z0952190E6PJC8Q98ZNX"
+}
+
+variable "alb_dns_name" {
+  description = "The DNS name of the ALB (output from ALB module)"
+  type        = string
+}
+
+variable "alb_hosted_zone_id" {
+  description = "The hosted zone ID of the ALB (output from ALB module)"
+  type        = string
 }
