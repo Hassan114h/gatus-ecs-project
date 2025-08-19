049199281b6adc84742785ae076bdcd4a5aa939a
Hassan <hassanhagi93@gmail.com>
2025-08-19 10:24:15 +0100

    fixing db issue

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -121,7 +121,7 @@ resource "aws_db_subnet_group" "memos" {
 resource "aws_db_instance" "memos" {
   allocated_storage    = 20
   engine               = "postgres"
-  engine_version       = "15.8"
+  engine_version       = "15.12"
   instance_class       = "db.t3.micro"
   username             = var.db_username
   password             = var.db_password
