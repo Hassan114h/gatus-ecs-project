7387c7c931b3391d0b19e8846621ebc1851acfa2
Hassan <hassanhagi93@gmail.com>
2025-08-19 15:50:53 +0100

    Fixing acm block

--- terraform/modules/route53/main.tf
+++ terraform/modules/route53/main.tf
@@ -9,11 +9,11 @@ resource "aws_acm_certificate" "cert" {
 }
 
 resource "aws_route53_record" "acm_cert_validation" {
-  zone_id = var.hosted_zone_id
-  name    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[0].resource_record_type
+  zone_id = var.alb_hosted_zone_id
+  name    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_type
   ttl     = 60
-  records = [aws_acm_certificate.cert.domain_validation_options[0].resource_record_value]
+  records = [tolist(aws_acm_certificate.cert.domain_validation_options)[0].resource_record_value]
 }
 
 resource "aws_acm_certificate_validation" "cert_validation" {
